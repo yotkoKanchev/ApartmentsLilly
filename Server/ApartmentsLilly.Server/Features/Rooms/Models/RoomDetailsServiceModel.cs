c2093fdb7b0d560e3e14675a2b254f9621515e63
yotkoKanchev <yotko.kanchev@gmail.com>
2020-11-22 12:19:57 +0200

    Implement all Room endpoints

---
+++ Server/ApartmentsLilly.Server/Features/Rooms/Models/RoomDetailsServiceModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ApartmentsLilly.Server.Features.Rooms.Models
+{
+    using Data.Models.Rooms;
+    using Infrastructure.Mapping;
+
+    public class RoomDetailsServiceModel : IMapFrom<Room>
+    {
+        public string Id { get; set; }
+
+        public bool IsSleepable { get; set; }
+
+        public RoomType RoomType { get; set; }
+
+        public int ApartmentId { get; set; }
+    }
+}
---
+++ Server/ApartmentsLilly.Server/Features/Rooms/Models/UpdateRoomRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ApartmentsLilly.Server.Features.Rooms.Models
+{
+    using Data.Models.Rooms;
+
+    public class UpdateRoomRequestModel
+    {
+        public bool IsSleepable { get; set; }
+
+        public RoomType RoomType { get; set; }
+    }
+}
