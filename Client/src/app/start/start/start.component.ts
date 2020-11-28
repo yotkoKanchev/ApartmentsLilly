a3d4f6d7f23c23edc4209d948af7b8b0b5e82986
yotkoKanchev <yotko.kanchev@gmail.com>
2020-11-28 23:50:20 +0200

    Add date validations

--- Client/src/app/start/start/start.component.ts
+++ Client/src/app/start/start/start.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { AnyARecord } from 'dns';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApartmentsService } from 'src/app/apartments/apartments.service';
+import { ApartmentListingModel } from 'src/app/apartments/models/apartment-listing.model';
 
 @Component({
   selector: 'app-start',
@@ -8,11 +9,52 @@ import { AnyARecord } from 'dns';
   styleUrls: ['./start.component.css']
 })
 export class StartComponent implements OnInit {
-  searchApartmentForm : FormGroup
-  constructor() { }
+  searchApartmentForm: FormGroup
+  apartments: Array<ApartmentListingModel>
+  minDate = new Date();
+  maxDate = new Date(this.minDate.getMonth() + 6)
+  constructor(private apartmentsService: ApartmentsService, private fb: FormBuilder) {
+    this.searchApartmentForm = this.fb.group({
+      'Start-date': ['',],
+      'End-date': ['',],
+      'Adults': ['', [Validators.min(1), Validators.max(3)]],
+      'Children': ['', [Validators.min(0), Validators.max(2)]],
+      'Infants': [',', [Validators.min(0), Validators.max(2)]]
+    },
+      { validator: [this.dateLessThan('Start-date', 'End-date'), this.datePassed('Start-date')] }
+     );
+  }
 
   ngOnInit(): void {
   }
 
-  onSubmit(){}
+  onSubmit() {
+    this.apartmentsService.getApartments().subscribe(data => {
+      this.apartments = data;
+    })
+  }
+
+  dateLessThan(from: string, to: string) {
+    return (group: FormGroup): { [key: string]: any } => {
+      let f = group.controls[from];
+      let t = group.controls[to];
+      if (f.value > t.value) {
+        return {
+          dates: "Date from should be less than Date to"
+        };
+      }
+      return {};
+    }
+  }
+  datePassed(date: string) {
+    return (group: FormGroup): { [key: string]: any } => {
+      let t = group.controls[date];
+      if (new Date(t.value) < new Date()) {
+        return {
+          dates: "Date can not be passed"
+        };
+      }
+      return {};
+    }
+  }
 }
